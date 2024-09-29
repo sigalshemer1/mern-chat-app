8c51a9eec9dd79438bd9107b29cd3250aad120f3
siaglshemer6 <sigal.shemer68@gmail.com>
2024-09-30 00:06:32 +0100

    socket.io

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-		  const newSocket = io("http://localhost:5000", {
+		  const newSocket = io("https://chat-app-prod-p3cq.onrender.com", {
 			query: { userId: authUser._id },
 		  });
 	  
